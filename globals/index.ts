8b888d32a3e74c585ab047feaf744d52b1695cdb
Paul Jack <134734881+p-jack@users.noreply.github.com>
2026-04-29 14:26:48 -0700

    initial commit

---
+++ globals/index.ts
@@ -0,0 +1,18 @@
+interface Provider {
+  win():Window
+  doc():Document
+}
+
+export const styleSheet = new CSSStyleSheet()
+
+export const state = new Map<string,object>()
+
+const providers:Provider = {
+  win:()=>window,
+  doc:()=>document,
+}
+
+export function setProviders(win:()=>Window, doc:()=>Document) {
+  providers.win = win
+  providers.doc = doc
+}
